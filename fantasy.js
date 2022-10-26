bc422fecbefe7e86179b4b664efcfc74c4530f1e
Jason Gillanders <jgilland@ualberta.ca>
2022-10-25 21:03:25 -0600

    attempting to get all players stats and save to each object

--- fantasy.js
+++ fantasy.js
@@ -1,17 +1,37 @@
-allPlayers = []
-allTeams = []
+var allPlayers = []
+var allTeams = []
 
 class Team {
-	constructor(name, id) {
+	constructor(name, id, schedule) {
 		this.name = name;
 		this.id = id;
+		this.schedule = schedule;
 	}
 }
 
 class Player {
-	constructor(name, id) {
+	constructor(name, id, position, goals, assists, gamesPlayed, pim, shots, blocks, hits, faceoffPct, shifts, ppp, shp, plusMinus) {
 		this.name = name;
 		this.id = id;
+		this.position = position;
+		this.goals = goals;
+		this.assists = assists;
+		this.points = this.goals + this.assists;
+		this.gamesPlayed = gamesPlayed;
+		this.pim = pim;
+		this.shots = shots;
+		this.blocks = blocks;
+		this.hits = hits;
+		this.faceoffPct = faceoffPct;
+		this.shifts = shifts;
+		this.ppp = ppp;
+		this.shp = shp;
+		this.plusMinus = plusMinus;
+		this.faceoffWon = this.faceoffPct*this.shifts;
+	}
+
+	getID() {
+		return this.id;
 	}
 }
 
@@ -46,6 +66,7 @@ function fantasy() {
 	reset();
     getAllTeamIds();
     getAllPlayerLinks();
+	getPlayerStats();
 }
 
 function getAllTeamIds() {
@@ -66,11 +87,41 @@ function getAllPlayerLinks() {
 		.then((response) => {
             for(i=0;i<allTeams.length;i++) {
                 for(j=0;j<response.data.teams[i].roster.roster.length;j++) {
-                    allPlayers[(j*i)+j] = new Player(response.data.teams[i].roster.roster[j].person.fullName,response.data.teams[i].roster.roster[j].person.id);
+                    allPlayers[(j*i)+j] = new Player(response.data.teams[i].roster.roster[j].person.fullName,response.data.teams[i].roster.roster[j].person.id,response.data.teams[i].roster.roster[j].position.code);
                 }
             }
             console.log(allPlayers)
         })
+    hideLoading();
+}
+
+function getPlayerStats() {
+    displayLoading();
+	for(i=0;i<allPlayers.length;i++) {
+		axios.get('https://statsapi.web.nhl.com/' + 'people/' + toString(allPlayers[i].getId()) + '/stats?stats=statsSingleSeason&season=20222023')
+		.then((response) => {
+			console.log("GOT HEREEEEE");
+			allPlayers[i].gamesPlayed = response.data.stats[0].splits[0].stat.games;
+			allPlayers[i].goals = response.data.stats[0].splits[0].stat.goals;
+			allPlayers[i].assists = response.data.stats[0].splits[0].stat.assists;
+			allPlayers[i].plusMinus = response.data.stats[0].splits[0].stat.plusMinus;
+			allPlayers[i].pim = response.data.stats[0].splits[0].stat.pim;
+			allPlayers[i].ppp = response.data.stats[0].splits[0].stat.powerPlayPoints;
+			allPlayers[i].shp = response.data.stats[0].splits[0].stat.shortHandedPoints;
+			allPlayers[i].shots = response.data.stats[0].splits[0].stat.shots;
+			allPlayers[i].faceoffPct = response.data.stats[0].splits[0].stat.faceOffPct;
+			allPlayers[i].shifts = response.data.stats[0].splits[0].stat.shifts;
+			if (allPlayers[i].position == 'C') {
+				allPlayers[i].faceoffWon = faceoffPct*shifts;
+			}
+			else {
+				allPlayers[i].faceoffWon = 0;
+			}
+			allPlayers[i].hits = response.data.stats[0].splits[0].stat.hits;
+			allPlayers[i].blocks = response.data.stats[0].splits[0].stat.blocked;
+        })
+	}
+	console.log(allPlayers)
     hideLoading();
 }
 
